5a9f029a468beb1f68e851e2fa28ad81e344bf08
Estudiantes <estudiantes@Mac-mini-de-Estudiantes.local>
2018-04-21 18:25:01 -0500

    primero

---
+++ Clase02/clase02.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var var2 : String =  "Nueva Variable"
+
+str = "nuevo nombre"
+
+var edad : Int = 34
+
+var masViejo = edad + 345
+
+let constante : String = "Esto no se puede Cambiar"
+
+var verdadero : Bool = false
+
+let PI : Double = 3.1415
+
---
+++ Clase02/ejercicios.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+class myClass{
+    var arreglo : [String] = ["uno","dos","tres","cuatro"]
+    
+    func buscar	(valor : String	)->String{
+        var encontrado:String = ""
+        for(ivalor,i) in arreglo{
+            if(ivalor==valor){
+                encontrado = ivalor
+            }
+            
+        }
+    }
+}
+
+
+//{}[String]
